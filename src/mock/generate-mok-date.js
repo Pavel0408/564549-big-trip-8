19632a06ba6fc3206b554b5a4dcf9976db673c0c
Pavel <pavel0408@gmail.com>
2019-03-01 12:02:27 +0300

    улучшил читаемость generate-mok-date

--- src/mock/generate-mok-date.js
+++ src/mock/generate-mok-date.js
@@ -1,18 +1,20 @@
 import {
   getRandomNumber
 } from './../utilities';
+
 export const gnerateMockDate = () => {
-  const maxTimeLength = 24 * 60 * 60 * 1000;
-  const MS_IN_WEEK = 7 * 24 * 60 * 60 * 1000;
-  const msInHour = 60 * 60 * 1000;
-  const msInMinute = 60 * 1000;
+  const MAX_TIME_LENGTH = 1000 * 60 * 60 * 24;
+  const MS_IN_WEEK = 1000 * 60 * 60 * 24 * 7;
+  const MS_IN_HOUR = 1000 * 60 * 60;
+  const MS_IN_MINUTE = 60 * 1000;
+
   const dateNow = new Date();
   const start = new Date(getRandomNumber(+dateNow, +dateNow + MS_IN_WEEK));
-  const end = new Date(getRandomNumber(+start, +start + maxTimeLength));
+  const end = new Date(getRandomNumber(+start, +start + MAX_TIME_LENGTH));
 
   const intervalInMs = (+end - +start);
-  const hours = Math.floor(intervalInMs / msInHour);
-  const minutes = Math.floor(intervalInMs % msInHour / msInMinute);
+  const hours = Math.floor(intervalInMs / MS_IN_HOUR);
+  const minutes = Math.floor(intervalInMs % MS_IN_HOUR / MS_IN_MINUTE);
   const interval = {
     hours,
     minutes
