e86b74437e2c3d8d56995f133bf6a2bcdd1bba8a
Pavel <pavel0408@gmail.com>
2019-03-09 12:04:50 +0300

    токчки отрисовываются из классов

---
+++ src/Point.js
@@ -0,0 +1,11 @@
+export class Point {
+  constructor(data) {
+    this._title = data.title;
+    this._type = data.type;
+    this._offers = data.offers;
+    this._description = data.description;
+    this._time = data.time;
+    this._price = data.price;
+    this._element = null;
+  }
+}
