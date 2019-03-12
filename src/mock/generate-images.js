dfc9a7818e2134e77f7140011b0cf57b94990db9
Pavel <pavel0408@gmail.com>
2019-03-12 12:22:30 +0300

    убрал цифры в константы

--- src/mock/generate-images.js
+++ src/mock/generate-images.js
@@ -3,12 +3,17 @@ import {
 } from "../utilities";
 
 export const generateImages = () => {
-  const imagesNumber = getRandomNumber(1, 4);
+  const IMAGE_HEIGHT = 133;
+  const MAX_IMAGE_WIDTH = 400;
+  const MIN_IMAGE_WIDTH = 44;
+  const MAX_IMAGES_NUMBER = 4;
+  const MIN_IMAGES_NUMBER = 1;
+
+  const imagesNumber = getRandomNumber(MIN_IMAGES_NUMBER, MAX_IMAGES_NUMBER);
   return new Array(imagesNumber)
     .fill(``)
     .map(() => {
-      const width = Math.floor(getRandomNumber(44, 400));
-      const height = 133;
-      return `<img src="http://picsum.photos/${width}/${height}?r=${Math.random()}" alt="picture from place" class="point__destination-image">`;
+      const imageWidth = Math.floor(getRandomNumber(MIN_IMAGE_WIDTH, MAX_IMAGE_WIDTH));
+      return `<img src="http://picsum.photos/${imageWidth}/${IMAGE_HEIGHT}?r=${Math.random()}" alt="picture from place" class="point__destination-image">`;
     });
 };
