b5a1ea25a42e46e7371c157c64b22eab3f861d95
Pavel <pavel0408@gmail.com>
2019-03-14 12:18:47 +0300

    сделал обработчик reset прибиндиванием функции в конструкторе

--- src/point-edit.js
+++ src/point-edit.js
@@ -20,11 +20,13 @@ export class PointEdit {
     this._price = data.price;
     this._element = null;
     this._submitHandler = null;
-    this._resetHandler = (evt) => {
-      evt.preventDefault();
-      this._unrender();
-    };
+    this._resetHandler = this._reset.bind(this);
     this._images = data.images;
+  }
+
+  _reset(evt) {
+    evt.preventDefault();
+    this._unrender();
   }
 
   get template() {
@@ -35,37 +37,28 @@ export class PointEdit {
           choose day
           <input class="point__input" type="text" placeholder="MAR 18" name="day">
         </label>
-
         <div class="travel-way">
           <label class="travel-way__label" for="travel-way__toggle">${pointsIcons[this._type]}</label>
-
           <input type="checkbox" class="travel-way__toggle visually-hidden" id="travel-way__toggle">
-
           <div class="travel-way__select">
             <div class="travel-way__select-group">
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-taxi" name="travel-way" value="taxi">
               <label class="travel-way__select-label" for="travel-way-taxi">${pointsIcons.Taxi} taxi</label>
-
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-bus" name="travel-way" value="bus">
               <label class="travel-way__select-label" for="travel-way-bus">${pointsIcons.Bus} bus</label>
-
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-train" name="travel-way" value="train">
               <label class="travel-way__select-label" for="travel-way-train">${pointsIcons.Train} train</label>
-
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-flight" name="travel-way" value="train" checked>
               <label class="travel-way__select-label" for="travel-way-flight">${pointsIcons.Flight} flight</label>
             </div>
-
             <div class="travel-way__select-group">
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-check-in" name="travel-way" value="check-in">
               <label class="travel-way__select-label" for="travel-way-check-in">${pointsIcons.Check} check-in</label>
-
               <input class="travel-way__select-input visually-hidden" type="radio" id="travel-way-sightseeing" name="travel-way" value="sight-seeing">
               <label class="travel-way__select-label" for="travel-way-sightseeing">${pointsIcons.Sightseeing} sightseeing</label>
             </div>
           </div>
         </div>
-
         <div class="point__destination-wrap">
           <label class="point__destination-label" for="destination">Flight to</label>
           <input class="point__destination-input" list="destination-select" id="destination" value="Chamonix" name="destination">
@@ -76,37 +69,30 @@ export class PointEdit {
             <option value="hotel"></option>
           </datalist>
         </div>
-
         <label class="point__time">
           choose time
           <input class="point__input" type="text" value="${ addLeadingZero(this._time.start.getHours())}:${addLeadingZero(this._time.start.getMinutes())} — ${addLeadingZero(this._time.end.getHours())}:${addLeadingZero(this._time.end.getMinutes())}" name="time" placeholder="${ addLeadingZero(this._time.start.getHours())}:${addLeadingZero(this._time.start.getMinutes())} —${addLeadingZero(this._time.end.getHours())}:${addLeadingZero(this._time.end.getMinutes())}">
         </label>
-
         <label class="point__price">
           write price
           <span class="point__price-currency">€</span>
           <input class="point__input" type="text" value="${this._price}" name="price">
         </label>
-
         <div class="point__buttons">
           <button class="point__button point__button--save" type="submit">Save</button>
           <button class="point__button" type="reset">Delete</button>
         </div>
-
         <div class="paint__favorite-wrap">
           <input type="checkbox" class="point__favorite-input visually-hidden" id="favorite" name="favorite">
           <label class="point__favorite" for="favorite">favorite</label>
         </div>
       </header>
-
       <section class="point__details">
         <section class="point__offers">
           <h3 class="point__details-title">offers</h3>
-
           <div class="point__offers-wrap">
           ${formatOffersEdit(this._offers)}
           </div>
-
         </section>
         <section class="point__destination">
           <h3 class="point__details-title">Destination</h3>
