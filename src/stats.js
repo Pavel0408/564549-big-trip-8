1a10a869d46dae250beec51c2f5e839cb5d39b0d
Pavel <pavel0408@gmail.com>
2019-03-27 21:39:52 +0300

    разделил функцию статистики

--- src/stats.js
+++ src/stats.js
@@ -1,5 +1,3 @@
-import Chart from "chart.js";
-import ChartDataLabels from "chartjs-plugin-datalabels";
 import {
   getPoints
 } from "./get-points-arr";
@@ -8,12 +6,25 @@ import {
   pointsIcons
 } from "./mock/mock-constants";
 
-export const stats = () => {
-  const moneyCtx = document.querySelector(`.statistic__money`);
-  const transportCtx = document.querySelector(`.statistic__transport`);
-  const timeSpendCtx = document.querySelector(`.statistic__time-spend`);
+import {
+  renderTransportStats,
+  renderMoneyStats
+} from "./render-stats";
 
+export const stats = () => {
   const pointArr = getPoints();
+  const statsСontainer = document.querySelector(`#stats`);
+  statsСontainer.innerHTML = `<div class="statistic__item statistic__item--money">
+  <canvas class="statistic__money" width="900"></canvas>
+</div>
+
+<div class="statistic__item statistic__item--transport">
+  <canvas class="statistic__transport" width="900"></canvas>
+</div>
+
+<div class="statistic__item statistic__item--time-spend">
+  <canvas class="statistic__time-spend" width="900"></canvas>
+</div>`;
 
   const transportTypes = [
     `taxi`,
@@ -70,141 +81,6 @@ export const stats = () => {
 
   const monyLabeslArr = generateLabelsArr(moneySet);
 
-  // Рассчитаем высоту канваса в зависимости от того, сколько данных в него будет передаваться
-  const BAR_HEIGHT = 55;
-  moneyCtx.height = BAR_HEIGHT * 6;
-  transportCtx.height = BAR_HEIGHT * 4;
-  timeSpendCtx.height = BAR_HEIGHT * 4;
-
-  // eslint-disable-next-line no-unused-vars
-  const moneyChart = new Chart(moneyCtx, {
-    plugins: [ChartDataLabels],
-    type: `horizontalBar`,
-    data: {
-      labels: monyLabeslArr,
-      datasets: [{
-        data: moneyDataArr,
-        backgroundColor: `#ffffff`,
-        hoverBackgroundColor: `#ffffff`,
-        anchor: `start`
-      }]
-    },
-    options: {
-      plugins: {
-        datalabels: {
-          font: {
-            size: 13
-          },
-          color: `#000000`,
-          anchor: `end`,
-          align: `start`,
-          formatter: (val) => `€ ${val}`
-        }
-      },
-      title: {
-        display: true,
-        text: `MONEY`,
-        fontColor: `#000000`,
-        fontSize: 23,
-        position: `left`
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: `#000000`,
-            padding: 5,
-            fontSize: 13,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          barThickness: 44,
-        }],
-        xAxes: [{
-          ticks: {
-            display: false,
-            beginAtZero: true,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          minBarLength: 50
-        }],
-      },
-      legend: {
-        display: false
-      },
-      tooltips: {
-        enabled: false,
-      }
-    }
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const transportChart = new Chart(transportCtx, {
-    plugins: [ChartDataLabels],
-    type: `horizontalBar`,
-    data: {
-      labels: transportLabelsArr,
-      datasets: [{
-        data: transportDataArr,
-        backgroundColor: `#ffffff`,
-        hoverBackgroundColor: `#ffffff`,
-        anchor: `start`
-      }]
-    },
-    options: {
-      plugins: {
-        datalabels: {
-          font: {
-            size: 13
-          },
-          color: `#000000`,
-          anchor: `end`,
-          align: `start`,
-          formatter: (val) => `${val}x`
-        }
-      },
-      title: {
-        display: true,
-        text: `TRANSPORT`,
-        fontColor: `#000000`,
-        fontSize: 23,
-        position: `left`
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: `#000000`,
-            padding: 5,
-            fontSize: 13,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          barThickness: 44,
-        }],
-        xAxes: [{
-          ticks: {
-            display: false,
-            beginAtZero: true,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          minBarLength: 50
-        }],
-      },
-      legend: {
-        display: false
-      },
-      tooltips: {
-        enabled: false,
-      }
-    }
-  });
+  renderMoneyStats(monyLabeslArr, moneyDataArr);
+  renderTransportStats(transportLabelsArr, transportDataArr);
 };
